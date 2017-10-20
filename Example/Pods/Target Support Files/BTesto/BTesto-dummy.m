3fb982321514d25762927f8a90e86fde27987713
apple <caiyuan@kuaifazs.com>
2017-10-20 18:43:21 +0800

    Version 0.1.1

---
+++ Example/Pods/Target Support Files/BTesto/BTesto-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BTesto : NSObject
+@end
+@implementation PodsDummy_BTesto
+@end
---
+++ Example/Pods/Target Support Files/Pods-BTesto_Example/Pods-BTesto_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BTesto_Example : NSObject
+@end
+@implementation PodsDummy_Pods_BTesto_Example
+@end
